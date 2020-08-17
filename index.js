6e56c919082c37f39ae2693475074d4d194e412b
manuel <manuelczs@outlook.com>
2020-08-17 20:45:13 -0300

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const server = require('express')();
 const bodyParser = require('body-parser');
-const cors = require('cors')
-const router = require('./app/routes/routes');
+const cors = require('cors');
+const router = require('./routes/routes');
 const displayRoutes = require('express-routemap');
 
-
-server.use(cors())
-server.use(bodyParser.json())
-server.use(router);
+server.use(cors());
+server.use(bodyParser.json());
+server.set('view engine', 'ejs');
+server.use('/', router());
 
 const port = 3000;
 server.listen(port, () => {
-    console.log(`API running -> Port ${port}`);
-    displayRoutes(server);
-
+  console.log(`API running -> Port ${port}`);
+  displayRoutes(server);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
 
-module.exports = router;
+module.exports = () => {
+  router.get('/', (req, res) => {
+    res.render('index');
+  });
+
+  router.get('/about', (req, res) => {
+    res.render('about');
+  });
+
+  router.get('/projects', (req, res) => {
+    res.render('projects');
+  });
+
+  router.get('/blog', (req, res) => {
+    res.render('blog');
+  });
+
+  router.get('/contactus', (req, res) => {
+    res.render('contactus');
+  });
+
+  return router;
+};
