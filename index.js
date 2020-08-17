9aaa7c76943cd662b8df7c803ee1de8d57588f64
fedegomezavalos@gmail.com <fedegomezavalos@gmail.com>
2020-08-17 19:42:24 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const server = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const router = require('./app/routes/routes');
+const displayRoutes = require('express-routemap');
+
+
+server.use(cors())
+server.use(bodyParser.json())
+server.use(router);
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`API running -> Port ${port}`);
+    displayRoutes(server);
+
+});
